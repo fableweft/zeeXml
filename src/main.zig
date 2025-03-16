7cb606137d82805ab2be1ff5ac3b8eb30e28aaf3
fableweft <adokojedi@gmail.com>
2025-03-16 11:03:14 +0300

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -8,7 +8,7 @@ pub fn main() !void {
     var xml_files_dir = try std.fs.cwd().openDir("xml-files", .{});
     defer xml_files_dir.close();
 
-    const file = try xml_files_dir.openFile("15mb.xml", .{});
+    const file = try xml_files_dir.openFile("138-KB-XML-File.xml", .{});
     defer file.close();
 
     const reader = file.reader();
